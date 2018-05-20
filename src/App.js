a74a78daaa6d701c677c051ee7a89f7a335b8725
Matthew Kelly <coolbeandroid@gmail.com>
2018-05-20 18:23:37 +1000

    Add digital font for display

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Safe from "./components/Safe";
 const App = () => (
   <div>
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet"/>
     <Safe/>
   </div>
 );
